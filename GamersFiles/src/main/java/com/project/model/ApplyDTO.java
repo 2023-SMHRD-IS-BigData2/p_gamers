b46e29460892e8c5ce3a45ebb4f2b379aea00b79
정찬호 <hiierm@naver.com>
2023-11-23 16:43:48 +0900

    dtd

--- GamersFiles/src/main/java/com/project/model/ApplyDTO.java
+++ GamersFiles/src/main/java/com/project/model/ApplyDTO.java
@@ -3,13 +3,15 @@ package com.project.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class ApplyDTO {
+	
 	
 	private String send_id;
 	private String send_nick;
@@ -18,7 +20,11 @@ public class ApplyDTO {
 	private String send_class;
 	private String p_date;
 	
-	
-	
-
+	public ApplyDTO(String send_id, String rcv_id, String send_pos, String send_class, String send_nick) {
+		this.send_id = send_id;
+		this.rcv_id = rcv_id;
+		this.send_pos = send_pos;
+		this.send_class = send_class;
+		this.send_nick = send_nick;
+	}
 }
